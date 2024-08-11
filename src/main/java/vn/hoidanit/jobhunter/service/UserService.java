0024a08e9edbd8b4c3456b0200a6c6297ce390da
TaiDT <taidt@splus-software.com.vn>
2024-08-11 17:38:34 +0700

    feature/pagination

--- src/main/java/vn/hoidanit/jobhunter/service/UserService.java
+++ src/main/java/vn/hoidanit/jobhunter/service/UserService.java
@@ -3,9 +3,13 @@ package vn.hoidanit.jobhunter.service;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import vn.hoidanit.jobhunter.domain.User;
+import vn.hoidanit.jobhunter.domain.dto.Meta;
+import vn.hoidanit.jobhunter.domain.dto.ResultPaginationDTO;
 import vn.hoidanit.jobhunter.repository.UserRepositoty;
 
 @Service
@@ -37,8 +41,21 @@ public class UserService {
         return this.userRepositoty.findByEmail(username);
     }
 
-    public List<User> handleGetAllUser() {
-        return this.userRepositoty.findAll();
+    public ResultPaginationDTO handleGetAllUser(Pageable pageable) {
+        Page<User> pageUser = this.userRepositoty.findAll(pageable);
+        ResultPaginationDTO rs = new ResultPaginationDTO();
+        Meta mt = new Meta();
+
+        mt.setPage(pageUser.getNumber());
+        mt.setPageSize(pageUser.getSize());
+
+        mt.setPages(pageUser.getTotalPages());
+        mt.setTotal(pageUser.getTotalElements());
+
+        rs.setMeta(mt);
+        rs.setResult(pageUser.getContent());
+        
+        return rs;
     }
 
     public User handleUpdateUser(User reqUser) {
